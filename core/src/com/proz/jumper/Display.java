cdaa546f9cbfc82199aedcc381e8b781c54b33e7
piropaolo <piropaolo27@gmail.com>
2017-04-21 11:41:35 +0200

    Change Sprite class usage to TextureRegion

--- core/src/com/proz/jumper/Display.java
+++ core/src/com/proz/jumper/Display.java
@@ -2,10 +2,9 @@ package com.proz.jumper;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-import static com.proz.jumper.TextureManager.platformSprite;
-import static com.proz.jumper.TextureManager.playerJumpSprite;
-import static com.proz.jumper.TextureManager.playerStandSprite;
-
+import static com.proz.jumper.TextureManager.platformRegion;
+import static com.proz.jumper.TextureManager.playerJumpRegion;
+import static com.proz.jumper.TextureManager.playerStandRegion;
 
 /**
  * Created by volterra on 15.04.17.
@@ -13,17 +12,14 @@ import static com.proz.jumper.TextureManager.playerStandSprite;
 public class Display {
     public static void displayPlayer(Player player, SpriteBatch batch){
         if (player.getAirborne() || player.getJump()){
-            playerJumpSprite.setPosition(player.getX(), player.getY());
-            playerJumpSprite.draw(batch);
+            batch.draw(playerJumpRegion, player.getX(), player.getY());
         }
         else {
-            playerStandSprite.setPosition(player.getX(), player.getY());
-            playerStandSprite.draw(batch);
+            batch.draw(playerStandRegion, player.getX(), player.getY());
         }
     }
 
     public static void displayPlatform(Platform platform, SpriteBatch batch){
-        platformSprite.setPosition(platform.getX(), platform.getY());
-        platformSprite.draw(batch);
+        batch.draw(platformRegion, platform.getX(), platform.getY());
     }
 }
--- core/src/com/proz/jumper/TextureManager.java
+++ core/src/com/proz/jumper/TextureManager.java
@@ -2,29 +2,19 @@ package com.proz.jumper;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 /**
  * Created by volterra on 15.04.17.
  */
 public class TextureManager {
-    public static Texture playerStandImage;
-    public static Sprite playerStandSprite;
-
-    public static Texture playerJumpImage;
-    public static Sprite playerJumpSprite;
-
-    public static Texture platformImage;
-    public static Sprite platformSprite;
+    public static TextureRegion playerStandRegion;
+    public static TextureRegion playerJumpRegion;
+    public static TextureRegion platformRegion;
 
     public static void load(){
-        playerStandImage = new Texture(Gdx.files.internal("p1_stand.png"));
-        playerStandSprite = new Sprite(playerStandImage);
-
-        playerJumpImage = new Texture(Gdx.files.internal("p1_jump.png"));
-        playerJumpSprite = new Sprite(playerJumpImage);
-
-        platformImage = new Texture(Gdx.files.internal("platform.png"));
-        platformSprite = new Sprite(platformImage);
+        playerStandRegion = new TextureRegion(new Texture(Gdx.files.internal("p1_stand.png")));
+        playerJumpRegion = new TextureRegion(new Texture(Gdx.files.internal("p1_jump.png")));
+        platformRegion = new TextureRegion(new Texture(Gdx.files.internal("platform.png")));
     }
 }
